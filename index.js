b0f6cf4abdd8b1400837cae33ec86ce06e92ddad
mfernandez7 <30597352+mfernandez7@users.noreply.github.com>
2017-10-11 17:10:26 -0400

    Final Project

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+	//on click event
+ 	$('#quote1').click(author1);
+ 	$('#quote2').click(author2);
+
+
+ 	// hide quote 1st step
+	function hideQuote1() {
+		$('quote1').hide();
+	};
+
+	function hideQuote2() {
+		$('quote2').hide();
+	}
+
+
+	// add authors' names 
+	function author1() {
+		hideQuote1();
+		$('#quote1').html('- Max Kellerman');
+	};
+
+	function author2() {
+		hideQuote2();
+		$('#quote2').html('- Grant Hughes');
+	};
+
+
+});
